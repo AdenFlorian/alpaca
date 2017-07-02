2b8b322e25365b90142e62905d630ada5841fdf0
David Valachovic <AdenFlorian@gmail.com>
2017-07-01 19:30:46 -0500

    coolor space plane mats, gravity gene

---
+++ Assets/PlanetGravity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NetObjGene))]
+public class PlanetGravity : MonoBehaviour
+{
+    public float gravity;
+	NetObjGene _netObjGene;
+	Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		_netObjGene = GetComponent<NetObjGene>();
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (_netObjGene.IsLocalPlayer)
+        {
+            var fromMeToOrigin = (Vector3.zero - transform.position).normalized;
+
+            var gravityForce = fromMeToOrigin * gravity * Time.fixedDeltaTime;
+
+            _rigidbody.AddForce(gravityForce, ForceMode.VelocityChange);
+        }
+    }
+}
